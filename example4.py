17d625c2834d047f9ddb411adbf1b6441522c293
Ahmet Behdioğlu <ahmetuestcostaj@gmail.com>
2025-04-19 19:28:10 +0300

    type conversion

---
+++ example4.py
@@ -0,0 +1,29 @@
+x = input("1. number: ")
+y = input("6. number: ")
+
+print(type(x))
+total = int(x) + int(y)
+
+print(total)
+
+print("\n")
+
+z= 5
+print(type(z))
+zzz = float(z)
+print(type(zzz))
+print(zzz)
+
+print("\n")
+
+# Solution of the example one
+
+pi = 3.14
+r = input("Diameter: ")
+area = pi * (float(r) ** 2)
+circumference = 2 * pi * float(r)
+
+print("Area: ",area)
+print("Circumference: ", circumference)
+print("\n")
+print("Area: " + str(area) + " " + "Circumference: " + str(circumference))
